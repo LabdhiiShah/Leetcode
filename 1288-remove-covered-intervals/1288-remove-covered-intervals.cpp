d63dc1556fffa10c6b175f84162f1caf4b0be91b
LabdhiiShah <labdhishah1608@gmail.com>
2026-07-06 21:21:48 +0530

    Time: 3 ms (42.52%), Space: 15.1 MB (60.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -15,7 +15,7 @@ public:
 
         for(int i = 1; i < intervals.size(); i++)
         {
-            if(result.back()[0] <= intervals[i][0] && result.back()[1] >= intervals[i][1])
+            if(result.back()[1] >= intervals[i][1])
                 continue; 
             result.push_back(intervals[i]);
         }
